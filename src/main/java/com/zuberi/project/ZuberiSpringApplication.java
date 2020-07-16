267fdf5d095c37b3f7df4cf1c4b1339c9b51418f
ZuberiD <63861198+ZuberiD@users.noreply.github.com>
2020-07-16 16:28:34 -0400

    Add files via upload

---
+++ src/main/java/com/zuberi/project/ZuberiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.zuberi.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuberiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuberiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zuberi/project/ZuberiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuberi.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuberiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
